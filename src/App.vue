e3a10fb968baaa965cfae1f163699e5d39dd85b4
Kartik Kumar Choudhary <choudharykartik87@gmail.com>
2019-07-23 19:25:54 -0700

    done

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Advertisment Website"/>
+    <img alt="Vue logo" src="./assets/sell.png">
+    <div><button v-on:click="seen = true">Click to Sell your product here</button></div>
+    <div v-if="seen">
+      <br>
+      <div>
+        <label for="name">Name</label>
+        <input style="margin-left:80px" id="name" v-model="name" type="text" name="name">
+      </div>
+      <br>
+      <div>
+        <label for="contact">Contact</label>
+        <input style="margin-left:70px" id="contact" v-model="contact" type="text" name="contact">
+      </div>
+      <br>
+      <div>
+        <label for="itemName">Item Name</label>
+        <input style="margin-left:45px" id="itemName" v-model="itemName" type="text" name="itemName">
+      </div>
+      <br>
+      <div>
+        <label for="itemDesc">Item Description</label>
+        <input style="margin-left:10px" id="itemDesc" v-model="itemDesc" type="text" name="itemDesc">
+      </div>
+      <br>
+      <div>
+        <button v-on:click="checkForm()">Submit</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,6 +39,21 @@ export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  data: function () {
+    return {
+      seen : false,
+      name : null,
+      itemName : null,
+      itemDesc : null,
+      contact : null
+
+    }
+  },
+  methods:{
+    checkForm: function () {
+      console.log(this.name+' is selling '+this.itemName+':'+this.itemDesc+'. Please contact: '+this.contact)
+    }
   }
 }
 </script>
